b65531ed1ebaf1f1b4f6709fb2978ff33604a292
Ujjal Banerjee <ujjalbanbbl@gmail.com>
2020-08-24 10:18:08 +0530

    ikl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,10 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = false
 
+  c
+
+  config.serve_static_files = true 
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -18,15 +22,18 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+  config.assets.enabled = true 
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled =true
+  config.public_file_server.enabled = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile =false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
